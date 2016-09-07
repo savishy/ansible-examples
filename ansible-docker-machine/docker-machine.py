0f13ac6648f0ee79f8da07c22acdbb833f2c6cc7
Vish <savishy.pv@gmail.com>
2016-09-07 15:49:55 +0530

    - modified the original to spit out info on all docker machines
    NOTE: WIP

---
+++ ansible-docker-machine/docker-machine.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+"""
+
+Ansible dynamic inventory that gets the docker-machine host and it's
+variables into the inventory to the 'docker-host' group.
+
+Example Usage:
+
+> eval "$(docker-machine env MACHINE_NAME)"
+$ ansible-playbook playbook.yml -i docker-machine.py
+
+Debug:
+
+python docker-machine.py
+
+"""
+
+import os
+import argparse
+import subprocess
+
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+
+"""
+    Gets information about a docker machine.
+    docker-machine host
+"""
+def getDockerMachineInfo(machineName, format):
+    return subprocess.check_output([
+        "docker-machine",
+        "inspect",
+        "-f",
+        format,
+        machineName ]).strip()
+
+
+"""
+    Gets all available docker machines.
+    Returning the list of available docker machines
+"""
+def getAllDockerMachines():
+    return subprocess.check_output([
+        "docker-machine",
+        "ls",
+        "-q" ]).strip()
+
+
+"""
+    Get the docker machine inventory group.
+    Returns the docker host group
+"""
+def getAnsibleDockerInventoryGroup(dmName):
+
+    return {
+        "hosts": ["localhost"],
+        "vars": {
+
+            # at the moment docker-machine is always using this port
+            "docker_host_url": "tcp://{}:2376".format(
+                getDockerMachineInfo(dmName, "{{.Driver.IPAddress}}")),
+
+            "docker_host_cert_path": getDockerMachineInfo(dmName, "{{.HostOptions.AuthOptions.StorePath}}"),
+
+            "ansible_connection": "local"
+        }
+    }
+
+
+# get the current docker machine name
+docker_machine_name = os.environ['DOCKER_MACHINE_NAME']
+if docker_machine_name is None:
+    raise Exception('Missing DOCKER_MACHINE_NAME environment variable')
+
+json_data = {
+    # "local": {
+    #     "hosts": ["localhost"],
+    #     "vars": { "ansible_connection": "local" }
+    # }
+}
+for line in getAllDockerMachines().split('\n'):
+    docker_machine_name = line
+    json_data[line] = getAnsibleDockerInventoryGroup(docker_machine_name)
+    
+print json.dumps(json_data, indent=4)
+    
